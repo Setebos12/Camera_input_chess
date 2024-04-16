50dd148c55637b68df2b3b996f4e6f2f726ed437
Setebos12 <140965218+Setebos12@users.noreply.github.com>
2024-04-16 15:55:44 +0200

    Update run_game.py

--- myproject/run_game.py
+++ myproject/run_game.py
@@ -6,13 +6,13 @@ import cv2
 from game import game
 models = []
 classes = []
-models.append(load_model("/Users/krzysztofrutkowski/Desktop/Models1/keras_model.h5", compile=False))
-classes.append(open("/Users/krzysztofrutkowski/Desktop/Models1/labels.txt", "r").readlines())
-models.append(load_model("/Users/krzysztofrutkowski/Desktop/Models2/keras_model.h5", compile=False))
-classes.append(open("/Users/krzysztofrutkowski/Desktop/Models2/labels.txt", "r").readlines())
-im = Image.open("/Users/krzysztofrutkowski/Downloads/Obrazek3.png")
+models.append(load_model("Path to model1", compile=False))
+classes.append(open("Path to classes1", "r").readlines())
+models.append(load_model("Path to model2", compile=False))
+classes.append(open("Path to classes2", "r").readlines())
+im = Image.open("png images")
 images = crop_image(im)
 np.set_printoptions(suppress=True)
 camera = cv2.VideoCapture(0)
-api_token = "lip_7kIouXh4yvanAQQXoaP2"
+api_token = "lichess_api_key"
 game(api_token,models,classes,images,camera)
